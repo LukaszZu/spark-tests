22d37ba10f15cd0696e299e15a0648108e20151f
Lukasz Zukowski <lz.zukowski@gmail.com>
2016-04-07 21:32:59 +0200

    flow definition

--- IntegrationDemo/src/main/java/com/example/flows/BasicFlows.java
+++ IntegrationDemo/src/main/java/com/example/flows/BasicFlows.java
@@ -8,8 +8,15 @@ import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.dsl.channel.MessageChannels;
 import org.springframework.integration.stream.CharacterStreamReadingMessageSource;
 import org.springframework.integration.stream.CharacterStreamWritingMessageHandler;
+import org.springframework.integration.ws.SimpleWebServiceOutboundGateway;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHeaders;
+import org.springframework.ws.soap.SoapVersion;
+import org.springframework.ws.soap.saaj.SaajSoapMessageFactory;
+
+import javax.xml.soap.MessageFactory;
+import javax.xml.soap.SOAPConstants;
+import javax.xml.soap.SOAPException;
 
 /**
  * Created by zulk on 07.04.16.
@@ -19,15 +26,30 @@ public class BasicFlows {
 
     @Bean
     public MessageChannel input() {
-        return MessageChannels.direct("ala").get();
+        return MessageChannels.direct().get();
     }
 
     @Bean
     public IntegrationFlow flow1() {
-        return IntegrationFlows.from(input()).transform(a -> {
+        return IntegrationFlows.from(input())
+                .handle(gateway())
+                .<String,String>transform(a -> {
             System.out.println(a);
             return a;
         }).handle(CharacterStreamWritingMessageHandler.stdout()).get();
+    }
+
+
+    private SimpleWebServiceOutboundGateway gateway() {
+        SimpleWebServiceOutboundGateway simpleWebServiceOutboundGateway = new SimpleWebServiceOutboundGateway("http://www.webservicex.com/globalweather.asmx");
+        SaajSoapMessageFactory          saajSoapMessageFactory          = new SaajSoapMessageFactory();
+        try {
+            saajSoapMessageFactory.setMessageFactory(MessageFactory.newInstance(SOAPConstants.SOAP_1_2_PROTOCOL));
+        } catch (SOAPException e) {
+            throw new RuntimeException(e);
+        }
+        simpleWebServiceOutboundGateway.setMessageFactory(saajSoapMessageFactory);
+        return  simpleWebServiceOutboundGateway;
     }
 
 }
